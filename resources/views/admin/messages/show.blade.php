6b2e30e5f23e236d12ef109f25883a984a347e6b
NBTech <nbtech.git@gmail.com>
2026-07-19 11:05:45 +0100

    fix(admin): texto botao 'Eliminar' apenas

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -66,7 +66,7 @@
                 <input type="hidden" name="redirect_to_next" value="1">
                 <button type="button" class="btn-danger" @click="if (confirm('Eliminar esta mensagem e ir para a seguinte?')) $refs.deleteForm.submit()">
                     <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"><path d="M3 6h18"/><path d="M8 6V4h8v2"/><path d="M19 6l-1 14H6L5 6"/><path d="M10 11v6M14 11v6"/></svg>
-                    Eliminar e seguinte
+                    Eliminar
                 </button>
             </form>
         </div>
